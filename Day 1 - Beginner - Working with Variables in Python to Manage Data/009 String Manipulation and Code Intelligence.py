6121f0e8770ffa182aa227ab3fe2ddaa1e594371
tathoy <jessica.innovative7@gmail.com>
2024-06-09 23:41:38 +0600

    day-01
    - 009 is completed
    - String Manipulation

---
+++ Day 1 - Beginner - Working with Variables in Python to Manage Data/009 String Manipulation and Code Intelligence.py
@@ -0,0 +1,17 @@
+print("Hello World")
+print("Hello World")
+print("Hello World")
+print("Hello World")
+#To make space between two lin we need to write print().it also another way to do the same work, just to put'\ n' end of the line
+print("**************************************")
+#bellow I used '\ n' to print multi line in a single line print!"
+print("**************************************")
+print("Hello World\nHello World\nHello World\nHello World\n")
+print("************************************")
+print(4*"Hello World\n")
+#string concat
+print("Hello" +" "+"Jessica")
+#Two more way to make space between string 
+print("Hello " + "Jessica")
+print("Hello" + " Jessica")
+
