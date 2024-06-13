c89dfd6783ed49f71cbf9f455ce6b38ea080b75f
tathoy <jessica.innovative7@gmail.com>
2024-06-13 22:07:06 +0600

    Day-1 completed
    all the video from day one completed

---
+++ Day 1 - Beginner - Working with Variables in Python to Manage Data/013 (extra challange)Python Variables.py
@@ -0,0 +1,11 @@
+#(without taking extra variable, need to swap the value)👇
+a=int(input("enter the value of a:"))
+b=int(input("enter the value of b:"))
+
+
+a=a+b
+b=a-b
+a=a-b
+
+print('a=' , a)
+print('b=' , b)
---
+++ Day 1 - Beginner - Working with Variables in Python to Manage Data/017 Day 1 Project Band Name Generator.py
@@ -0,0 +1,27 @@
+#1. Create a greeting for your program.
+
+#2. Ask the user for the city that they grew up in.
+
+#3. Ask the user for the name of a pet.
+
+#4. Combine the name of their city and pet and show them their band name.
+
+#5. Make sure the input cursor shows on a new line:
+
+# Solution: https://replit.com/@appbrewery/band-name-generator-end
+
+#using "f" string .......
+
+'''
+Welcome to the Band Name Generator.
+What's the name of the city you grew up in?
+Dhaka
+What's your pet's name?
+Jojo Jenny
+Your band name could be Dhaka Jojo Jenny'''
+
+print("Welcome to the Band Name Generator.")
+city_name=input("What's the name of the city you grew up in?")
+pet_name=input("What's your pet's name?")
+print(f"Your band name could be {city_name} {pet_name}" )
+
