b0374c99e98fff68fcaa16a939488099f9efad37
tathoy <jessica.innovative7@gmail.com>
2024-06-14 23:19:23 +0600

    002 Python Primitive Data Types
    - Data types

---
+++ 02 - Day 2 - Beginner - Understanding Data Types and How to Manipulate Strings/002 Python Primitive Data Types.py
@@ -0,0 +1,15 @@
+#string
+string_message= "Hello"
+print (f"message is {string_message} and the type is {type(string_message)}" )
+
+#Integer
+integer_number= 10_00_000
+print(f"value is {integer_number} and the type is {type(integer_number)}" )
+
+#Float
+float_number=234.3482
+print(f"value is {float_number} and the type is {type(float_number)}")
+
+#Boolean
+True
+False
