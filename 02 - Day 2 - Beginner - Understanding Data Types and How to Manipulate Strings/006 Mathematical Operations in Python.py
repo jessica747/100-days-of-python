a5e64f001a2492750c86bc6d95ca0d03e0af18a4
tathoy <jessica.innovative7@gmail.com>
2024-06-16 22:31:58 +0600

    Day-2
    - PEMDAS

---
+++ 02 - Day 2 - Beginner - Understanding Data Types and How to Manipulate Strings/006 Mathematical Operations in Python.py
@@ -0,0 +1,16 @@
+
+
+'''
+#PEMDAS
+P=Parentheses  ()
+E=Exponents  **
+M=Multiplication *
+D=Division  /
+A=Addition  +
+S=Subtraction -
+'''
+
+# print (3*3+3/3-3)
+#print (3*3/3-3+3)
+print (3*(3+3)/3-3)
+
