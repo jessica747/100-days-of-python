cff07fb681dd3e080d4e2ebc8d8c32f522be8529
tathoy <jessica.innovative7@gmail.com>
2024-06-21 23:23:13 +0600

    Day-3
    -003 Control Flow with if  else and Conditional Operators

---
+++ 03 - Day 3 - Beginner - Control Flow and Logical Operators/003 Control Flow with if  else and Conditional Operators.py
@@ -0,0 +1,6 @@
+print("Welcome to the roller coster!")
+height=int(input("What is height in cm?"))
+if height > 120 :
+    print("You can ride the roller coster!")
+else:
+    print("Sorry , you have to grow taller before you ride !")
